7fd4693266c3f4af40fcf2dbfa428d011b3ee0b1
Marco Hutter <javagl@javagl.de>
2023-04-23 04:19:43 +0200

    Work around 'unknown' in better-sqlite2 types

--- src/packages/TilesetPackage3dtiles.ts
+++ src/packages/TilesetPackage3dtiles.ts
@@ -29,6 +29,52 @@ export class TilesetPackage3dtiles implements TilesetPackage {
       throw new TilesetPackageError("Database already opened");
     }
     this.db = new DatabaseConstructor(fullInputName);
+
+    const message = TilesetPackage3dtiles.validateTableStructure(this.db!);
+    if (defined(message)) {
+      this.close();
+      throw new TilesetPackageError(message!);
+    }
+  }
+
+  /**
+   * Validates that the structure of the given database matches
+   * the requirements of a 3D Tiles database:
+   *
+   * There must be a table called 'media'.
+   * The table must have 2 columns.
+   * The name of column 0 must be 'key'
+   * The type of column 0 must be 'TEXT'
+   * The name of column 1 must be 'content'
+   * The type of column 1 must be 'BLOB'
+   *
+   * @param db - The database
+   * @returns An error message, or `undefined` if the structure is valid
+   */
+  private static validateTableStructure(db: Database): string | undefined {
+    let selection;
+    try {
+      selection = db.prepare("SELECT * FROM media LIMIT 1");
+    } catch (e) {
+      return `${e}`;
+    }
+    const columns = selection.columns();
+    if (columns.length !== 2) {
+      return `Expected 2 columns, but found ${columns.length}`;
+    }
+    if (columns[0].name !== "key") {
+      return `Column 0 name must be 'key', but is ${columns[0].name}`;
+    }
+    if (columns[0].type !== "TEXT") {
+      return `Column 0 type must be 'TEXT', but is ${columns[0].type}`;
+    }
+    if (columns[1].name !== "content") {
+      return `Column 1 name must be 'content', but is ${columns[1].name}`;
+    }
+    if (columns[1].type !== "BLOB") {
+      return `Column 1 type must be 'BLOB', but is ${columns[1].type}`;
+    }
+    return undefined;
   }
 
   getKeys(): IterableIterator<string> {
@@ -39,7 +85,7 @@ export class TilesetPackage3dtiles implements TilesetPackage {
     }
     const selection = this.db!.prepare("SELECT * FROM media");
     const iterator = selection.iterate();
-    return Iterables.map(iterator, (row) => row.key);
+    return Iterables.map(iterator, (row: any) => row.key);
   }
 
   getEntry(key: string): Buffer | undefined {
@@ -47,7 +93,7 @@ export class TilesetPackage3dtiles implements TilesetPackage {
       throw new Error("Package is not opened. Call 'open' first.");
     }
     const selection = this.db!.prepare("SELECT * FROM media WHERE key = ?");
-    const row = selection.get(key);
+    const row = selection.get(key) as any;
     if (defined(row)) {
       return row.content;
     }
